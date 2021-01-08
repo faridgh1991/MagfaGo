431c77c4c0815086821365540661f710a8c3ee0c
FaridGh <faridgh1991@gmail.com>
2021-01-08 18:12:12 +0330

    Implement Send Method

---
+++ send.go
@@ -0,0 +1,57 @@
+package magfa
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type SendRequest struct {
+	Senders    []string `json:"senders,omitempty"`
+	Recipients []string `json:"recipients,omitempty"`
+	Messages   []string `json:"messages,omitempty"`
+	Encodings  []int    `json:"encodings,omitempty"`
+	Uids       []int64  `json:"uids,omitempty"`
+	Udhs       []string `json:"udhs,omitempty"`
+}
+
+type SendResult struct {
+	Messages []SendResponse
+	Status   int
+}
+
+type SendResponse struct {
+	Status     int
+	Id         int64
+	UserId     int64
+	Parts      int
+	Tariff     float64
+	Alphabet   string
+	Recipient  string
+	StatusText string
+}
+
+func (c *Client) Send(request SendRequest) ([]SendResponse, error) {
+
+	body, err := json.Marshal(request)
+	raw, err := c.sendRequest("send", http.MethodPost, body)
+	if err != nil {
+		return nil, fmt.Errorf("send failed: %s", err.Error())
+	}
+
+	var resp SendResult
+	err = json.Unmarshal(raw, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal response: %s", err.Error())
+	}
+	if resp.Status != 0 {
+		return nil, newError(resp.Status)
+	}
+	for i, msg := range resp.Messages {
+		if m, ok := errorMap[msg.Status]; ok {
+			resp.Messages[i].StatusText = m
+		}
+	}
+
+	return resp.Messages, nil
+}
